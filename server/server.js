af82ceece44c906e1a701e8715e19f0882cd97e9
C Bares <colinedwinbares@gmail.com>
2022-05-24 19:17:13 -0500

    set up src folder, cleaned up boilerplate, added favicon

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
