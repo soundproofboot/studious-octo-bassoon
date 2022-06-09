56a8f0aca2c2e8189536c0cb1113261cc436d950
C Bares <colinedwinbares@gmail.com>
2022-06-08 19:28:36 -0500

    changes to background and basic layout of image and text box on home

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,12 +1,27 @@
-import './Home.css'
+import './Home.css';
+import photo from '../../assets/professional-photo.jpg'
 
 const Home = () => {
-  return (
-    <section className='main-section' id='home'>
-    <h2 id='logo'>Logo?</h2>
-    <p id='intro'>This is your landing page. What would you like your prospective clients to see/know/learn about you when they first arrive here?</p>
-    </section>
-  )
-}
 
-export default Home
+  return (
+    <section className="main-section" id="home">
+      <div id="image-container">
+        <img src={photo} alt="Marie Humes" className="photo"></img>
+      </div>
+
+      <div id="intro-container">
+        <h2 id="tagline">Maybe a tagline?</h2>
+        <p id="intro-text">
+          Tell your clients something about yourself. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Curabitur iaculis lobortis
+          ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et
+          ultrices posuere cubilia curae; Ut auctor, ipsum at porttitor varius,
+          urna felis ultrices eros, at facilisis urna nisl nec erat. Vestibulum
+          aliquet arcu vitae lectus bibendum consectetur. Nunc eget.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
